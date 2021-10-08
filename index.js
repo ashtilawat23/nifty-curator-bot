0ebb36767b4269f8a2be85a7505cfc76f35a17dd
ashtilawat23 <ashtilawat23@gmail.com>
2021-10-07 21:47:22 -0400

    Loading html logic complete.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const {TwitterClient} = require('twitter-api-client');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const pretty = require('pretty');
 
 
 const client = new TwitterClient({
@@ -10,4 +11,16 @@ const client = new TwitterClient({
     accessToken: process.env.ACCESS_TOKEN,
     accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
 });
+
+let rarityHomePageMarkup = '';
+
+axios.get('https://opensea.io/rankings?category=new')
+  .then((res) => {
+    const $ = cheerio.load(res.data);
+    const itemsCenter = pretty($('.Overflowreact__OverflowContainer-sc-10mm0lu-0 fqMVjm RankingsTableRowPrimaryreact__RankingsCollectionPageOverflow-sc-2buw7n-0 egizGf').html());
+    console.log(itemsCenter);
+  })
+  .catch( (err) => {
+    console.log(err);
+  });
 
