164d85f349e1e5ce2124467545d1462db995977c
ashtilawat23 <ashtilawat23@gmail.com>
2021-10-09 19:12:29 -0400

    Tried puppeteer, but not as good as Cheerio.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 const {TwitterClient} = require('twitter-api-client');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const pretty = require('pretty');
-
+const puppeteer = require('puppeteer');
 
 const client = new TwitterClient({
     apiKey: process.env.CONSUMER_API_KEY, 
@@ -11,16 +8,3 @@ const client = new TwitterClient({
     accessToken: process.env.ACCESS_TOKEN,
     accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
 });
-
-let rarityHomePageMarkup = '';
-
-axios.get('https://opensea.io/rankings?category=new')
-  .then((res) => {
-    const $ = cheerio.load(res.data);
-    const itemsCenter = pretty($('.Overflowreact__OverflowContainer-sc-10mm0lu-0 fqMVjm RankingsTableRowPrimaryreact__RankingsCollectionPageOverflow-sc-2buw7n-0 egizGf').html());
-    console.log(itemsCenter);
-  })
-  .catch( (err) => {
-    console.log(err);
-  });
-
